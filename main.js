161af5f5fe0fec43c19ec9d5e63cd5db33ef3dad
Utkarsh_Goyal <utkarshgoyal07@gmail.com>
2023-08-31 23:46:10 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text',{
+    strings:["College student","Frontend Developer"," Engineer"," Programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
